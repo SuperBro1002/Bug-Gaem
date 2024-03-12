7282261c22da8043fbd6b5e1de372045e2aa0f98
SuperBro1002 <126834262+SuperBro1002@users.noreply.github.com>
2024-03-11 17:10:57 -0700

    UI work
    - Portrait UI switches to current ally
    - Background added

--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -1,1 +1,14 @@
 extends Control
+
+func _ready():
+	SignalBus.connect("currentUnit", set_ui_portrait)
+
+func set_ui_portrait(unit):
+	if unit.get_faction() == unit.fac.ALLY:
+		var portraitRes = load("res://Assets/Sprites/Allies/" + unit.Name+"/"+unit.Name+"_Base_Still.png")
+		get_node("../UI/InfoBox/PortraitBox/Sprite2D").texture = portraitRes
+	
+
+
+func _on_sprite_2d_texture_changed():
+	print("CHANGED")
