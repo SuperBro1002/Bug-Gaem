55b515d587ab1bb23661605d2361ded9f11c3b96
SuperBro1002 <126834262+SuperBro1002@users.noreply.github.com>
2024-05-09 09:45:51 -0700

    Enemy Aggro Range
    - Enemies now have an editable # that determines their range for detecting targets. If no one is within that range, the enemy will not move or act that turn.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -7,6 +7,7 @@ var screenSize
 @onready var ability2 = load_ability(SetAbility1)
 @onready var ability3 = load_ability(SetAbility1)
 
+@export var aggroRange = 8
 var animationSpeed = 4
 var type = fac.ENEMY
 var lengthList = []
@@ -48,27 +49,29 @@ func unique_turn_start():
 			shortestPath = lengthList[i]
 			target = AutoloadMe.globalAllyList[i]
 	
-	end = grid.local_to_map(target.position)
-	
-	var pathArray = AutoloadMe.movementGrid.get_point_path(start, end)
-	
-	if canMove == true:
-		for i in pathArray.size() - 1:
-			if CurrentAP != 0 and i > 0:
-				CurrentAP -= 1
-				var tween = create_tween()
-				tween.tween_property(self, "position", pathArray[i], 1.0 / animationSpeed).set_trans(Tween.TRANS_SINE)
-				await tween.finished
-	
-	pathArray = AutoloadMe.movementGrid.get_point_path(grid.local_to_map(position), end)
-	
-	while CurrentAP >= ability1.get_ap_cost() and pathArray.size() <= 2:
-		ability1.targetUnits.append(target)
-		AutoloadMe.currentAbility = ability1
-		run_passives(methodType.ABILITY_EXECUTE, null)
-		ability1.execute()
-		CurrentAP -= ability1.get_ap_cost()
+	if shortestPath <= aggroRange + 1:
+		end = grid.local_to_map(target.position)
 		
-		await get_tree().create_timer(1).timeout
-	
+		var pathArray = AutoloadMe.movementGrid.get_point_path(start, end)
+		
+		if canMove == true:
+			for i in pathArray.size() - 1:
+				if CurrentAP != 0 and i > 0:
+					CurrentAP -= 1
+					var tween = create_tween()
+					tween.tween_property(self, "position", pathArray[i], 1.0 / animationSpeed).set_trans(Tween.TRANS_SINE)
+					await tween.finished
+		
+		pathArray = AutoloadMe.movementGrid.get_point_path(grid.local_to_map(position), end)
+		
+		while CurrentAP >= ability1.get_ap_cost() and pathArray.size() <= 2:
+			ability1.targetUnits.append(target)
+			AutoloadMe.currentAbility = ability1
+			run_passives(methodType.ABILITY_EXECUTE, null)
+			ability1.execute()
+			CurrentAP -= ability1.get_ap_cost()
+			
+			await get_tree().create_timer(1).timeout
+	else:
+		print("Short path ", shortestPath)
 	on_turn_end()
